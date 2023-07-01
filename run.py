e3e9bafe35d2efcb5f192ada795f11d277d8ee39
pengchengo <304829842@qq.com>
2023-07-01 17:46:52 +0800

    run

--- run.py
+++ run.py
@@ -2,9 +2,12 @@ import TikTokDownload as TK
 import os
 import re
 
-os.mkdir("下载")
+if not os.path.exists("下载"):
+    os.mkdir("下载")
 with open("downLoadList.txt") as file:
     for item in file:
         t = re.findall('(https://v.douyin.com/.*?/)', item, re.S)
         if len(t)!=0:
             os.system('python3 TikTokDownload.py -u '+ t[0] + ' -m yes')
+        else:
+            print("不是分享地址:"+item)
